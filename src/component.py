8eb67497e3072ac99474762d8200dc94f1ce5653
SgtMarmite <d.novotny31415@gmail.com>
2023-05-30 20:49:08 +0200

    raise UserException in main entrypoint

--- src/component.py
+++ src/component.py
@@ -472,8 +472,10 @@ class Component(ComponentBase):
 if __name__ == "__main__":
     try:
         comp = Component()
-        # this triggers the run method by default and is controlled by the configuration.action parameter
-        comp.run()
+        comp.execute_action()
+    except UserException as exc:
+        logging.exception(exc)
+        exit(1)
     except Exception as exc:
         logging.exception(exc)
         exit(2)
